ceb7863828dbe49bee4eb51e2510ef16274f428d
totanvix <totanvix@gmail.com>
2023-04-10 17:13:33 +0700

    fix: add validate

--- utils/shortener/shortener.go
+++ utils/shortener/shortener.go
@@ -18,14 +18,13 @@ const rawAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw
 func Do(chatId int, text string) {
 	arr := strings.Fields(text)
 	args := arr[1:]
-	if len(args) == 0 {
+
+	if len(args) != 1 {
 		bot.SendMessage(chatId, "Sử dụng cú pháp <code>/shortener https://example.com/</code> để tạo rút gọn liên kết")
 		return
 	}
 
 	url := text[11:]
-	fmt.Println(url)
-
 	if !isUrl(url) {
 		bot.SendMessage(chatId, "URL không đúng định dạng")
 		return
