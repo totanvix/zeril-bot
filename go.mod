cb4cf5aa2df37c754f2501963a46878982b4d113
totanvix <totanvix@gmail.com>
2023-04-10 11:31:54 +0700

    feat: upgrade lunnar command

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module zeril-bot
 go 1.20
 
 require (
-	github.com/Lofanmi/chinese-calendar-golang v0.0.0-20211214151323-ef5cb443e55e
 	github.com/leekchan/accounting v1.0.0
+	github.com/mmcdole/gofeed v1.2.1
+	github.com/oliamb/cutter v0.2.2
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 )
 
@@ -13,7 +14,6 @@ require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/cockroachdb/apd v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/mmcdole/gofeed v1.2.1 // indirect
 	github.com/mmcdole/goxpp v1.1.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
--- utils/lunar/lunar.go
+++ utils/lunar/lunar.go
@@ -1,27 +1,69 @@
 package lunar
 
 import (
+	"errors"
 	"fmt"
-	"strconv"
+	"image/jpeg"
+	"log"
+	"net/http"
+	"os"
 	"time"
 	"zeril-bot/utils/bot"
 
-	"github.com/Lofanmi/chinese-calendar-golang/calendar"
+	"github.com/oliamb/cutter"
 )
 
 func SendLunarDateNow(chatId int) {
-	t := time.Now()
-	c := calendar.ByTimestamp(t.Unix())
+	y, m, d := time.Now().Date()
 
-	d := c.Lunar.GetDay()
-	m := strconv.Itoa(int(c.Lunar.GetMonth()))
-	y := c.Lunar.GetYear()
+	path := "/tmp/lunar.jpg"
+	err := downloadAndCropImage(fmt.Sprintf("https://licham365.vn/images/lich-am-ngay-%v-thang-%v-nam-%v.jpg", d, int(m), y), path)
 
-	if len(m) == 1 {
-		m = "0" + m
+	bot.SendAPhoto(chatId, path)
+	log.Println(err)
+}
+
+func downloadAndCropImage(URL, fileName string) error {
+	//Get the response bytes from the url
+	response, err := http.Get(URL)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return errors.New("Received non 200 response code")
 	}
 
-	message := fmt.Sprintf("🗓 %v/%v/%v", d, m, y)
+	//Create a empty file
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-	bot.SendMessage(chatId, message)
+	img, err := jpeg.Decode(response.Body)
+	if err != nil {
+		return err
+	}
+
+	cImg, err := cutter.Crop(img, cutter.Config{
+		Height: 560,  // height in pixel or Y ratio(see Ratio Option below)
+		Width:  1200, // width in pixel or X ratio
+		// Mode:    cutter.Centered,     // Accepted Mode: TopLeft, Centered
+		// Anchor:  image.Point{10, 10}, // Position of the top left point
+		// Options: 0,                   // Accepted Option: Ratio
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if err := jpeg.Encode(file, cImg, &jpeg.Options{
+		Quality: 100,
+	}); err != nil {
+		return err
+	}
+
+	return nil
 }
