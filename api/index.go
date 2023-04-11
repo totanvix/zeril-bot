07ca2d4badd354c2e080916d08db48db9699d2ff
totanvix <totanvix@gmail.com>
2023-04-11 11:28:29 +0700

    change package name

--- api/index.go
+++ api/index.go
@@ -3,18 +3,18 @@ package api
 import (
 	"net/http"
 	"zeril-bot/api/hook"
-	"zeril-bot/api/request"
+	"zeril-bot/api/middleware"
 	"zeril-bot/api/url"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	chiMiddle "github.com/go-chi/chi/v5/middleware"
 )
 
 func Handler(wri http.ResponseWriter, req *http.Request) {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Use(request.PreRequest)
-	r.Use(request.Recoverer)
+	r.Use(chiMiddle.Logger)
+	r.Use(middleware.PreRequest)
+	r.Use(middleware.Recoverer)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
