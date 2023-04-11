b8ce9f9e852c10c523cfd2115301a9568ee92b7e
totanvix <totanvix@gmail.com>
2023-04-11 11:34:32 +0700

    test

--- api/index.go
+++ api/index.go
@@ -3,7 +3,7 @@ package api
 import (
 	"net/http"
 	"zeril-bot/api/hook"
-	"zeril-bot/api/testmiddle"
+	api "zeril-bot/api/testmiddle"
 	"zeril-bot/api/url"
 
 	"github.com/go-chi/chi/v5"
@@ -13,8 +13,8 @@ import (
 func Handler(wri http.ResponseWriter, req *http.Request) {
 	r := chi.NewRouter()
 	r.Use(chiMiddle.Logger)
-	r.Use(testmiddle.PreRequest)
-	r.Use(testmiddle.Recoverer)
+	r.Use(api.PreRequest)
+	r.Use(api.Recoverer)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
--- api/testmiddle/index.go
+++ api/testmiddle/index.go
@@ -1,4 +1,4 @@
-package testmiddle
+package api
 
 import (
 	"context"
